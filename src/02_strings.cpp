d9572cdb3beecd0cb1d385d93a61f13a9f8cab53
Qingyu Li <2310301201@stu.pku.edu.cn>
2025-12-03 21:28:29 +0800

    string: string_view

--- src/02_strings.cpp
+++ src/02_strings.cpp
@@ -7,9 +7,19 @@ int main() {
     auto name = ""s;
     // after 'cin', there will be a '\n' left, use std::ws
     std::getline(std::cin >> std::ws, name);
+
     // int nameLen = {static_cast<int>(name.length())};
     auto nameLen = std::ssize(name);
+
+    auto nameCopy = std::string_view(name); // without copying, but can't be modified
+
+    // string_view: string shouldn't be constructed inside
+
+    // constexpr: can be added at compile time
+    constexpr std::string_view msg = "Hello!";
+
     std::cout << "len: " << nameLen << '\n';
-    std::cout << "name: " << name << '\n';
+    std::cout << "name: " << nameCopy << '\n';
+    std::cout << "constexpr: " << msg << '\n';
     return 0;
 }
